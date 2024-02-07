56120787a8155da392ba9707137c71ab73785c22
Witsathon <6338200021@student.chula.ac.th>
2024-02-07 15:58:02 +0700

    Update NewsCard.js

--- finapp/src/component/news/NewsCard.js
+++ finapp/src/component/news/NewsCard.js
@@ -1,13 +1,13 @@
 import React from 'react';
-
+import './NewsCard.css';
 const NewCard = ({ image, title, description, date }) => {
   return (
-    <div className="new-card">
-      <img src={image} alt={title} className="new-card-image" />
-      <div className="new-card-body">
-        <h2 className="new-card-title">{title}</h2>
-        <p className="new-card-description">{description}</p>
-        <p className="new-card-date">{date}</p>
+    <div className="news-card">
+      <img src={image} className="news-card-image" />
+      <div className="news-card-body">
+        <h2 className="news-card-title">{title}</h2>
+        <p className="news-card-description">{description}</p>
+        <p className="news-card-date">{date}</p>
       </div>
     </div>
   );
