21bd608c52b3771aac26933164a5c9fd00cbbe80
sunnypawat <sunnypawat@gmail.com>
2024-02-18 23:10:12 +0700

    footer css component

--- finapp/src/component/main/Footer.js
+++ finapp/src/component/main/Footer.js
@@ -4,16 +4,18 @@ import './Footer.css';
 export default function Footer() {
   return (
     <div className='footer'>
-      <div className='container'>
-        <h2>Community</h2>
-        <h3>Facebook</h3>
-        <h3>Instagram</h3>
-        <h3>Twitter</h3>
-      </div>
-      <div className='container'>
-        <h2>More</h2>
-        <h3>About</h3>
-        <h3>Customer support</h3>
+      <div className='container-out'>
+        <div className='container'>
+          <h2>Community</h2>
+          <h3>Facebook</h3>
+          <h3>Instagram</h3>
+          <h3>Twitter</h3>
+        </div>
+        <div className='container'>
+          <h2>More</h2>
+          <h3>About</h3>
+          <h3>Customer support</h3>
+        </div>
       </div>
       <p>&copy; 2024 Ice-project. All rights reserved.</p>
     </div>
