0673185f4442c64603dcea6254ee9e5c5a6de3f0
Witsathon <6338200021@student.chula.ac.th>
2024-02-19 14:39:54 +0700

    Update MainNavbar.js

--- finapp/src/component/main/MainNavbar.js
+++ finapp/src/component/main/MainNavbar.js
@@ -8,6 +8,9 @@ import Login from '@/page/Login';
 import React, { useState } from 'react';
 import './MainNavbar.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
+
 
 export default function MainNavbar() {
   const [activeSection, setActiveSection] = useState('home');
@@ -60,7 +63,9 @@ export default function MainNavbar() {
         <button className={getButtonClass('chart')} onClick={() => navigateTo('chart')}>Chart</button>
         <button className={getButtonClass('market')} onClick={() => navigateTo('market')}>Market</button>
         <button className={getButtonClass('news')} onClick={() => navigateTo('news')}>News</button>
-        <button className={`${getButtonClass('login')} loginButton`} onClick={() => navigateTo('login')}>Login</button>
+        <button className={`${getButtonClass('login')} loginButton`} onClick={() => navigateTo('login')}>
+          <FontAwesomeIcon icon={faSignInAlt} /> Login
+        </button>
       </nav>
 
       <main>
