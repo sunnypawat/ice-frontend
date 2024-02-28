1bd19fe2a564c8a4d1d86f16d546587ff1eb1cc3
Witsathon <6338200021@student.chula.ac.th>
2024-02-28 13:39:16 +0700

    Update Home.js

--- finapp/src/page/Home/Home.js
+++ finapp/src/page/Home/Home.js
@@ -12,52 +12,56 @@ const marketData = [1, 1, 1];
 export default function Home() {
   return (
     <div id="home">
-        <Start />
-        <div className='newsView'>
-          <h1 className='newsStartTitles'>Recommended</h1>
-          <div className='newsContainer'>
-            {myArray.map((item, index) => (
-              <NewCard
-              id = {index}
+      <Start />
+      <div className='newsView'>
+        <h1 className='newsStartTitles'>Recommended</h1>
+        <div className='newsContainer'>
+          {myArray.map((item, index) => (
+            <NewCard
+              id={index}
               image="path-to-your-image.jpg"
               title="November Prediction by Experts"
               description="An analysis of market trends and predictions."
               date="10 November 2023"
-              />
-            ))}
-          </div>
-            
-          <h1>New Content</h1>
-          <div className='newsContainer'>
-            {myArray.map((item, index) => (
-              <NewCard
-              id = {index}
-              image="path-to-your-image.jpg"
-              title="November Prediction by Experts"
-              description="An analysis of market trends and predictions."
-              date="10 November 2023"
-              />
-            ))}
-          </div>
-            
-          <h1>News</h1>
-          <div className='newsContainer'>
-            {myArray.map((item, index) => (
-              <NewCard
-              id = {index}
-              image="path-to-your-image.jpg"
-              title="November Prediction by Experts"
-              description="An analysis of market trends and predictions."
-              date="10 November 2023"
-              />
-            ))}
-          </div>
-
-          <div className='marketInfoView'>
-          <MarketInfoCard data={marketData} />
-          </div>
-
+            />
+          ))}
         </div>
+
+        <h1>New Content</h1>
+        <div className='newsContainer'>
+          {myArray.map((item, index) => (
+            <NewCard
+              id={index}
+              image="path-to-your-image.jpg"
+              title="November Prediction by Experts"
+              description="An analysis of market trends and predictions."
+              date="10 November 2023"
+            />
+          ))}
+        </div>
+
+        <h1>News</h1>
+        <div className='newsContainer'>
+          {myArray.map((item, index) => (
+            <NewCard
+              id={index}
+              image="path-to-your-image.jpg"
+              title="November Prediction by Experts"
+              description="An analysis of market trends and predictions."
+              date="10 November 2023"
+            />
+          ))}
+        </div>
+
+        <div className='marketInfoCardsContainer'>
+          {Array(4).fill().map((_, index) => (
+            <div className='marketInfoView' key={index}>
+              <MarketInfoCard data={marketData} />
+            </div>
+          ))}
+        </div>
+
+      </div>
     </div>
   )
 }
