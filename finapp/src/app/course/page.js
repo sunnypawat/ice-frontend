ed203b7dac626c1c8b01e1b775d4b522b7ddcb91
Witsathon <6338200021@student.chula.ac.th>
2024-03-19 00:25:32 +0700

    Update page.js

--- finapp/src/app/course/page.js
+++ finapp/src/app/course/page.js
@@ -1,11 +1,15 @@
 import Image from 'next/image'
 import CourseBanner from '@/component/courseMain/CourseBanner'
+import NextButton from '@/component/courseMain/NextButton'
+import PreviousButton from '@/component/courseMain/PreviousButton'
 
 export default function Course() {
   return (
     <div id="home">
         <h1>Course Selection</h1>
         <p>Select some trading and investment courses</p>
+        <NextButton />
+        <PreviousButton />
         <CourseBanner 
           title='What is investment?' 
           description='The  great opportunity for you to understand fundamental and technical technique for investing' 
