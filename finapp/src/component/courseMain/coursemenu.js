adc6556ef44af2a12e9bdc62677f34532000f2eb
Witsathon <6338200021@student.chula.ac.th>
2024-03-27 17:12:25 +0700

    Create coursemenu.js

---
+++ finapp/src/component/courseMain/coursemenu.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './courseMenu.css';
+//import avatar from './path-to-your-image.jpg'; // Make sure to put the correct path to your image file here
+
+const CourseMenu = () => {
+  return (
+    <div className="course-menu">
+      <h2 className="course-header">Course Material</h2>
+      <ul className="course-sections">
+        <li>Section 1</li>
+        <li>Section 2</li>
+        <li>Section 3</li>
+      </ul>
+      <div className="course-bottom-links">
+        <div>Report</div>
+        <div>Course Info</div>
+      </div>
+    </div>
+  );
+};
+
+export default CourseMenu;
+
+///<img src={avatar} alt="Course Avatar" className="course-avatar" />
