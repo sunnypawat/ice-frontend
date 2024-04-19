a210c7a0d2b44bf6f66717c88a4ab38434cd4ce1
Witsathon <6338200021@student.chula.ac.th>
2024-04-19 21:35:33 +0900

    Update page.js

--- finapp/src/app/course/page.js
+++ finapp/src/app/course/page.js
@@ -1,17 +1,39 @@
+"use client"
 import Image from 'next/image'
 import CourseBanner from '@/component/courseMain/CourseBanner'
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 export default function Course() {
+  const [courses, setCourses] = useState([]); // State to store course data
+  const apiUrl = process.env.NEXT_PUBLIC_BASE_API_URL;
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      try {
+        const response = await axios.get(apiUrl+'/api/courses'); // Adjust URL as necessary
+        setCourses(response.data); // Assume response.data contains the array of courses
+      } catch (error) {
+        console.error('Failed to fetch courses:', error);
+      }
+    };
+
+    fetchCourses();
+  }, []);
+
   return (
     <div id="home">
         <h1>Course Selection</h1>
         <p>Select some trading and investment courses</p>
-        <CourseBanner 
-          title='What is investment?' 
-          description='The  great opportunity for you to understand fundamental and technical technique for investing' 
-          level='Level 1: Basic' 
-          access={false}
-        />
+        {courses.map((course) => (
+          <CourseBanner 
+            key={course.id} // Assume each course has a unique 'id'
+            title={course.title} 
+            description={course.description}
+            level='Level 1: Basic'
+            access={false} // Assume 'access' is a boolean
+          />
+        ))}
     </div>
-  )
+  );
 }
